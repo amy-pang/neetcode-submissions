b15f949ff884b7659d5a63b08fdf3232010848d3
Amy Pang <amypang2027@gmail.com>
2026-09-11 19:13:09 -0400

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+        print(nums_set)
+        longest_seq = 0
+        for n in nums:
+            m = n
+            if n - 1 not in nums_set: 
+                while m in nums_set:
+                    m += 1
+                if m - n > longest_seq:
+                    longest_seq = m - n
+        
+        return longest_seq
